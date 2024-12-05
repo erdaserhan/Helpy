cf6517f0c435e496254f1b70a7dbdbc0752269dd
erdaserhan <erhan209@gmail.com>
2024-12-05 21:19:48 +0100

    ok

--- assets/controllers/viewport_sidebar_controller.js
+++ assets/controllers/viewport_sidebar_controller.js
@@ -1,20 +1,22 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-    connect()
-    {
-        window.addEventListener("resize", this.updateViewportWidthSidebar.bind(this))
-        this.updateViewportWidthSidebar()
+    connect() {
+        this.updateViewportWidthSidebar = this.updateViewportWidthSidebar.bind(this); // Bind once
+        window.addEventListener("resize", this.updateViewportWidthSidebar);
+        this.updateViewportWidthSidebar();
     }
 
-    updateViewportWidthSidebar()
-    {
-        const sidebar = this.element
+    // disconnect() {
+    //     window.removeEventListener("resize", this.updateViewportWidthSidebar);
+    // }
 
+    updateViewportWidthSidebar() {
+        const sidebar = this.element;
         if (window.innerWidth <= 823) {
-            sidebar.classList.remove('show')
+            sidebar.classList.remove("show");
         } else {
-            sidebar.classList.add('show')
+            sidebar.classList.add("show");
         }
     }
 }
