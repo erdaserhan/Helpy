fb9e18e3d254bbf3ad7c1724928674808383db10
erdaserhan <erhan209@gmail.com>
2024-12-08 20:13:06 +0100

    ok

---
+++ src/Controller/InterventionsController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Intervention;
+use App\Form\InterventionsType;
+use App\Repository\InterventionRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/interventions')]
+final class InterventionsController extends AbstractController
+{
+    #[Route(name: 'app_interventions_index', methods: ['GET'])]
+    public function index(InterventionRepository $interventionRepository): Response
+    {
+        return $this->render('interventions/index.html.twig', [
+            'interventions' => $interventionRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_interventions_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $intervention = new Intervention();
+        $form = $this->createForm(InterventionsType::class, $intervention);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($intervention);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_interventions_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('interventions/new.html.twig', [
+            'intervention' => $intervention,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_interventions_show', methods: ['GET'])]
+    public function show(Intervention $intervention): Response
+    {
+        return $this->render('interventions/show.html.twig', [
+            'intervention' => $intervention,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_interventions_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Intervention $intervention, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(InterventionsType::class, $intervention);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_interventions_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('interventions/edit.html.twig', [
+            'intervention' => $intervention,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_interventions_delete', methods: ['POST'])]
+    public function delete(Request $request, Intervention $intervention, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$intervention->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($intervention);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_interventions_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
+    
---
+++ src/Form/InterventionsType.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Beneficiaire;
+use App\Entity\Intervention;
+use App\Entity\InterventionType;
+use App\Entity\Personnel;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class InterventionsType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('montantPaye')
+            ->add('dateFacture', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('montantRealise')
+            ->add('dateRealise', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('divers')
+            ->add('pieceNo')
+            ->add('extraitNo')
+            ->add('personnel', EntityType::class, [
+                'class' => Personnel::class,
+                'choice_label' => 'id',
+            ])
+            ->add('type', EntityType::class, [
+                'class' => self::class,
+                'choice_label' => 'id',
+            ])
+            ->add('beneficiaire', EntityType::class, [
+                'class' => Beneficiaire::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Intervention::class,
+        ]);
+    }
+}
